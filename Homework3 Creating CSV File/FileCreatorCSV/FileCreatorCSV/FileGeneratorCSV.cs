2e4bbf75d75a46e31802aaee4d90de350d51fc2d
rockfantop <artem.furmanov2002@gmail.com>
2021-05-11 13:12:26 +0300

    Homework3 is done.

---
+++ Homework3 Creating CSV File/FileCreatorCSV/FileCreatorCSV/FileGeneratorCSV.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace FileCreatorCSV
+{
+    class FileGeneratorCSV
+    {
+        private List<string> inputProperties = new List<string>();
+
+        private Type fileType;
+
+        private IEnumerable<IEntity> data;
+
+
+        public void SetFileType(Type type)
+        {
+            this.fileType = type;
+        }
+
+        public void SetData(IEnumerable<IEntity> data)
+        {
+            this.data = data;
+        }
+
+        public void SetProperties()
+        {
+            string[] properties = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries);
+            
+            foreach (var property in properties)
+            {
+                if (fileType.GetProperty(property.Trim()) != null)
+                {
+                    this.inputProperties.Add(property.Trim());
+                }
+            }
+        }
+
+        public void CreateFile()
+        {
+            try
+            {
+                using (StreamWriter streamWriter = new StreamWriter("..//..//..//file.csv", false, System.Text.Encoding.Default))
+                {
+                    foreach(var property in inputProperties)
+                    {
+                        streamWriter.Write(property + ",");
+                    }
+
+                    foreach (var entity in data)
+                    {
+                        streamWriter.WriteLine();
+
+                        foreach (var property in inputProperties)
+                        {
+                            var temp = entity.GetType().GetProperty(property).GetValue(entity, null);
+                            if (temp is decimal d)
+                            {
+                                streamWriter.Write(d.ToString(CultureInfo.CreateSpecificCulture("en-GB")) + ",");
+                            }
+                            else if (temp == null ? false : temp.ToString().Contains(','))
+                            {
+                                streamWriter.Write($"\"{temp.ToString()}\"" + ",");
+                            }
+                            else
+                            {
+                                streamWriter.Write(temp + ",");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Homework3 Creating CSV File/FileCreatorCSV/FileCreatorCSV/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileCreatorCSV
+{
+    public class Person : IEntity
+    {
+        public int Age { get; set; }
+
+        public string EyeColor { get; set; }
+
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Company { get; set; }
+
+        public string Address { get; set; }
+
+        public decimal? Salary { get; set; }
+    }
+
+}
