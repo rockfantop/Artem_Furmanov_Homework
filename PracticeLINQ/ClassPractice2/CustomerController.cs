0dc88af844022705c66291589a5b978266afd7b4
rockfantop <artem.furmanov2002@gmail.com>
2021-05-12 18:26:16 +0300

    LINQ practice is done.

---
+++ PracticeLINQ/ClassPractice2/CustomerController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ClassPractice2
+{
+    class CustomerController
+    {
+        private List<Customer> customers;
+
+        public CustomerController(List<Customer> customers)
+        {
+            this.customers = customers;
+        }
+
+        public Customer GetFirstRegistretedCustomer()
+        {
+            return customers.OrderBy(x => x.RegistrationDate).FirstOrDefault();
+        }
+
+        public double CustomersBalanceAverage()
+        {
+            return customers.Average(x => x.Balance);
+        }
+
+        public void CustomersFilterByDate(DateTime x, DateTime y)
+        {
+            customers = customers.Where(i => i.RegistrationDate > x && i.RegistrationDate < y).ToList();
+
+            if (customers == null)
+            {
+                Console.WriteLine("No results");
+            }
+        }
+
+        public void CustomersFilterById(long id)
+        {
+            customers = customers.Where(i => i.Id == id).ToList();
+        }
+
+        public void CustomersFilterByName(string name)
+        {
+            customers = customers.Where(i => i.Name.ToLower().Contains(name.ToLower())).ToList();
+        }
+
+        public void CustomersFilterByMonth()
+        {
+            customers = customers.OrderBy(i => i.RegistrationDate.Month).ThenBy(i => i.Name).ToList();
+        }
+
+        public void CustomersFilterByProperty(string property, string orderBy)
+        {
+            if (orderBy == "ascending")
+            {
+                customers = customers.OrderBy(i => i.GetType().GetProperty(property).GetValue(i)).ToList();
+            }
+            else
+            {
+                customers = customers.OrderByDescending(i => i.GetType().GetProperty(property).GetValue(i)).ToList();
+            }
+        }
+
+        public void PrintCustomers()
+        {
+            customers.ForEach(i => { Console.WriteLine(i.Name + i.RegistrationDate + ", "); });
+        }
+    }
+}
---
+++ PracticeLINQ/ClassPractice2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerController customerController = new CustomerController(new List<Customer>
+{
+    new Customer(1, "Tawana Shope", new DateTime(2017, 7, 15), 15.8),
+    new Customer(2, "Danny Wemple", new DateTime(2016, 2, 3), 88.54),
+    new Customer(3, "Margert Journey", new DateTime(2017, 11, 19), 0.5),
+    new Customer(4, "Tyler Gonzalez", new DateTime(2017, 5, 14), 184.65),
+    new Customer(5, "Melissa Demaio", new DateTime(2016, 9, 24), 241.33),
+    new Customer(6, "Cornelius Clemens", new DateTime(2016, 4, 2), 99.4),
+    new Customer(7, "Silvia Stefano", new DateTime(2017, 7, 15), 40),
+    new Customer(8, "Margrett Yocum", new DateTime(2017, 12, 7), 62.2),
+    new Customer(9, "Clifford Schauer", new DateTime(2017, 6, 29), 89.47),
+    new Customer(10, "Norris Ringdahl", new DateTime(2017, 1, 30), 13.22),
+    new Customer(11, "Delora Brownfield", new DateTime(2011, 10, 11), 0),
+    new Customer(12, "Sparkle Vanzile", new DateTime(2017, 7, 15), 12.76),
+    new Customer(13, "Lucina Engh", new DateTime(2017, 3, 8), 19.7),
+    new Customer(14, "Myrna Suther", new DateTime(2017, 8, 31), 13.9),
+    new Customer(15, "Fidel Querry", new DateTime(2016, 5, 17), 77.88),
+    new Customer(16, "Adelle Elfrink", new DateTime(2017, 11, 6), 183.16),
+    new Customer(17, "Valentine Liverman", new DateTime(2017, 1, 18), 13.6),
+    new Customer(18, "Ivory Castile", new DateTime(2016, 4, 21), 36.8),
+    new Customer(19, "Florencio Messenger", new DateTime(2017, 10, 2), 36.8),
+    new Customer(20, "Anna Ledesma", new DateTime(2017, 12, 29), 0.8)
+});
+            Console.WriteLine(customerController.GetFirstRegistretedCustomer().RegistrationDate);
+
+            Console.WriteLine(customerController.CustomersBalanceAverage());
+
+            //customerController.CustomersFilterByDate(new DateTime(2000, 01, 01), new DateTime(2017, 01, 01));
+
+            //customerController.CustomersFilterById(15);
+
+            //customerController.CustomersFilterByName("ad");
+
+            customerController.CustomersFilterByMonth();
+
+            //customerController.CustomersFilterByProperty("RegistrationDate", "ascending");
+
+            customerController.PrintCustomers();
+        }
+    }
+}
