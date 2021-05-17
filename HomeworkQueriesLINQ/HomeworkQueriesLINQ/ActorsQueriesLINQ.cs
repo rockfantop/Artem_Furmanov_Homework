ff494e94f595d5a769104f2c5df7f287bc82dd65
rockfantop <artem.furmanov2002@gmail.com>
2021-05-17 05:02:54 +0300

    Homework LINQ queries is done.

---
+++ HomeworkQueriesLINQ/HomeworkQueriesLINQ/ActorsQueriesLINQ.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeworkQueriesLINQ
+{
+    abstract class ArtObject
+    {
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+
+    class Film : ArtObject
+    {
+
+        public int Length { get; set; }
+        public IEnumerable<Actor> Actors { get; set; }
+    }
+
+    class Actor
+    {
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+
+    class Article : ArtObject
+    {
+        public int Pages { get; set; }
+    }
+
+
+    class ActorsQueriesLINQ
+    {
+        public void PrintAllNames(List<object> data)
+        {
+            data.OfType<Film>()
+                .Select(x => x.Actors).SelectMany(x => x).ToList()
+                .ForEach(x => { Console.WriteLine(x.Name); });
+        }
+
+        public void GetCountOfActors(List<object> data)
+        {
+            int count = data.OfType<Film>()
+                .Select(x => x.Actors).SelectMany(x => x.Where(y => y.Birthdate.Month == 8)).ToList().Count();
+            Console.WriteLine($"Actors burn in August: {count}");
+        }
+
+        public void PrintTwoOldestActors(List<object> data)
+        {
+            var actors = data.OfType<Film>()
+                .Select(x => x.Actors).SelectMany(x => x).OrderByDescending(x => x.Birthdate).ToList();
+            Console.WriteLine($"Two oldest actors is {actors[actors.Count - 1].Name} and {actors[actors.Count - 2].Name}");
+        }
+
+        public void PrintAuthorsActicles(List<object> data)
+        {
+            data.OfType<Article>()
+                .Select(x => x.Author).Distinct().ToList()
+                .ForEach(x => { Console.WriteLine($"{x} - {data.OfType<Article>().Count(y => y.Author == x)} articles"); });
+        }
+
+        public void PrintAuthorsOfArticlesAndFilms(List<object> data)
+        {
+            data.OfType<Article>()
+                .Select(x => x.Author).Distinct().ToList()
+                .ForEach(x => { Console.WriteLine($"{x} - {data.OfType<Article>().Count(y => y.Author == x)} articles"); });
+            data.OfType<Film>()
+                .Select(x => x.Author).Distinct().ToList()
+                .ForEach(x => { Console.WriteLine($"{x} - {data.OfType<Film>().Count(y => y.Author == x)} films"); });
+        }
+
+        public void PrintDistinctCharsOfActorsNames(List<object> data)
+        {
+            var chars = data.OfType<Film>()
+                .Select(x => x.Actors).SelectMany(x => x).Select(x => x.Name.ToLower()).Distinct()
+                .SelectMany(x => x).Distinct().ToList().Count() - 1;
+
+            Console.WriteLine($"{chars} different chars");
+        }
+
+        public void PrintArticleNames(List<object> data)
+        {
+            data.OfType<Article>().OrderBy(x => x.Author).ThenBy(x => x.Pages).ToList()
+                .ForEach(x => { Console.WriteLine(x.Name); });
+        }
+
+        public void PrintActorAndHisFilms(List<object> data)
+        {
+            data.OfType<Film>()
+                .Select(x => x.Actors).SelectMany(x => x).Select(x => x).Distinct().ToList()
+                .ForEach(x => 
+                {
+                    Console.Write(x.Name + ": ");
+                    data.OfType<Film>().Where(y => y.Actors.Contains(x)).Select(y => y.Name).ToList().ForEach(y =>
+                    {
+                        Console.Write(y + " ");
+                    });
+                    Console.WriteLine();
+                });
+        }
+
+        public void GetSum(List<object> data)
+        {
+            Console.WriteLine($@"Article pages: {data.OfType<Article>().Sum(x => x.Pages)}
+Sum of All: {data.OfType<Article>().Sum(x => x.Pages) + data.OfType<ArtObject>().Sum(x => x.Year) + data.OfType<Film>().Sum(x => x.Length)}");
+        }
+
+        public void GetDictionary(List<object> data)
+        {
+            var dictionary = data.OfType<Article>().Select(x => x.Author).Distinct()
+                .ToDictionary(x => x, x => data.OfType<Article>().Where(y => y.Author == x).ToList());
+        }
+    }
+}
---
+++ HomeworkQueriesLINQ/HomeworkQueriesLINQ/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HomeworkQueriesLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First Part
+
+            var queries = new QuiriesWithDigitsAndStrings();
+
+            //queries.PrintDigits();
+
+            //queries.PrintDigitsDivisibleBy3();
+
+            //queries.PrintWord();
+
+            //queries.PrintAllStringsWithA("aaa;abb;ccc;dap");
+
+            //queries.PrintAllStringsAndCountOfA("aaa;abb;ccc;dap");
+
+            //queries.IsWordExist("aaa;abb;ccc;dap", "abb");
+
+            //queries.PrintLongestString("aaa; xabbx; abb; ccc; dap");
+
+            //queries.PrintAverageLenghtOfString("aaa; xabbx; abb; ccc; dap");
+
+            //queries.PrintShortestStringReverse("aaa; xabbx; abb; ccc; dap; zh");
+
+            //queries.PrintStringsStartsAEndsB("baaa; aabb; aaa; aabbx; abb; ccc; dap; zh");
+
+            //queries.PrintLastInSequence("baaa; aabb; aaa; aabbx; abb; ccc; dap; zh; asdbb; vasdasdbb; bbbbbbb");
+
+            /////
+            ///
+
+            //Second Part
+
+            List<object> data = new List<object> {
+                "Hello",
+                new Article { Author = "Hilgendorf", Name = "Punitive law and criminal law doctrine.", Pages = 44 },
+                new List<int> {45, 9, 8, 3},
+                new string[] {"Hello inside array"},
+                new Film { Author = "Martin Scorsese", Name= "The Departed", Actors = new List<Actor>() {
+                    new Actor { Name = "Jack Nickolson", Birthdate = new DateTime(1937, 4, 22)},
+                    new Actor { Name = "Leonardo DiCaprio", Birthdate = new DateTime(1974, 11, 11)},
+                    new Actor { Name = "Matt Damon", Birthdate = new DateTime(1970, 8, 10)}
+                }},
+                new Film { Author = "Gus Van Sant", Name = "Good Will Hunting", Actors = new List<Actor>() {
+                    new Actor { Name = "Matt Damon", Birthdate = new DateTime(1970, 8, 10)},
+                    new Actor { Name = "Robin Williams", Birthdate = new DateTime(1951, 8, 11)},
+                }},
+                new Article { Author = "Basov", Name="Classification and content of restrictive administrative measures applied in the case of emergency", Pages = 35},
+                new Article { Author = "Basov", Name="Classification and content of restrictive administrative measures applied in the case of emergency 2", Pages = 35},
+                "Leonardo DiCaprio"
+            };
+
+            var actorQueries = new ActorsQueriesLINQ();
+
+            actorQueries.PrintAllNames(data);
+
+            actorQueries.GetCountOfActors(data);
+
+            actorQueries.PrintTwoOldestActors(data);
+
+            //actorQueries.PrintAuthorsActicles(data);
+
+            actorQueries.PrintAuthorsOfArticlesAndFilms(data);
+
+            actorQueries.PrintDistinctCharsOfActorsNames(data);
+
+            actorQueries.PrintArticleNames(data);
+
+            actorQueries.PrintActorAndHisFilms(data);
+
+            actorQueries.GetSum(data);
+
+            actorQueries.GetDictionary(data);
+        }
+    }
+}
