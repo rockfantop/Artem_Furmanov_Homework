a8896c233f927cd24548ec7c0ece45e11869829a
rockfantop <artem.furmanov2002@gmail.com>
2021-05-23 15:22:55 +0300

    Deleted useless comments.

--- PracticeWithStringsAndАormatting/ClassPractice3/ClassPractice3/Program.cs
+++ PracticeWithStringsAndАormatting/ClassPractice3/ClassPractice3/Program.cs
@@ -8,27 +8,27 @@ namespace ClassPractice3
         {
             var tasks = new Tasks();
 
-            //tasks.GetDividedNumber(125, 13);
+            tasks.GetDividedNumber(125, 13);
 
-            //tasks.ReadExponentialNumber();  
+            tasks.ReadExponentialNumber();
 
-            //tasks.GetCurrentDate();
+            tasks.GetCurrentDate();
 
-            //Console.WriteLine(tasks.ParseDate("2016 21-07"));
+            Console.WriteLine(tasks.ParseDate("2016 21-07"));
 
-            //tasks.GetSumOfNumbersInString(Console.ReadLine());
+            tasks.GetSumOfNumbersInString(Console.ReadLine());
 
-            //tasks.FindSubString("asdfsadfasdf123213 asdasdasfasdfsad21324343543sdsdfsdfdf343453453");
+            tasks.FindSubString("asdfsadfasdf123213 asdasdasfasdfsad21324343543sdsdfsdfdf343453453");
 
-            //tasks.ValidatePassword("vasta");
+            tasks.ValidatePassword("vasta");
 
-            //tasks.ReplacePhoneNumbers("+380-98-123-45-67 vasya +380-95-381-00-49");
+            tasks.ReplacePhoneNumbers("+380-98-123-45-67 vasya +380-95-381-00-49");
 
-            //string[] names = { "vasya petin", "petro kvitka-jolud" };
+            string[] names = { "vasya petin", "petro kvitka-jolud" };
 
-            //names = tasks.NamesToUpper(names);
+            names = tasks.NamesToUpper(names);
 
-            //tasks.DecodeBase64String("0JXRgdC70Lgg0YLRiyDRh9C40YLQsNC10YjRjCDRjdGC0L7RgiDRgtC10LrRgdGCLCDQt9C90LDRh9C40YIg0LfQsNC00LDQvdC40LUg0LLRi9C/0L7Qu9C90LXQvdC+INCy0LXRgNC90L4gOik=");
+            tasks.DecodeBase64String("0JXRgdC70Lgg0YLRiyDRh9C40YLQsNC10YjRjCDRjdGC0L7RgiDRgtC10LrRgdGCLCDQt9C90LDRh9C40YIg0LfQsNC00LDQvdC40LUg0LLRi9C/0L7Qu9C90LXQvdC+INCy0LXRgNC90L4gOik=");
 
             double[] numbers = { 1.2, 2.3, 1, 0.3, 5};
             
